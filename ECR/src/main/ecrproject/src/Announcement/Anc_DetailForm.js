d70cf1942508da2b1cb42723d045569cee02708d
hyunminhwan <hmh3319@gmail.com>
2024-09-06 15:21:16 +0900

    0906 project

---
+++ ECR/src/main/ecrproject/src/Announcement/Anc_DetailForm.js
@@ -0,0 +1,98 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useLocation, useNavigate } from "react-router-dom";
+
+function Anc_DetailForm() {
+    const [announcement, setAnnouncement] = useState(null);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { boardNo } = location.state || {}; // boardNo를 location.state에서 가져옵니다.
+
+    useEffect(() => {
+        if (boardNo) {
+            axios.get(`/board/form/${boardNo}`)
+                .then((response) => {
+                    setAnnouncement(response.data);
+                })
+                .catch(() => {
+                    console.log("데이터 로드 실패");
+                });
+        }
+    }, [boardNo]);
+
+    if (!announcement) return <p>로딩 중...</p>;
+
+    const handleEditClick = (boardNo) => {
+        navigate(`/Anc_EditForm`,{state:{boardNo}});
+    };
+
+    return (
+        <>
+            <br/><br/>
+            <h1>게시판 상세조회</h1>
+            <br/><br/><br/><br/>
+            <form>
+                <table align='center'>
+                    <tbody>
+                        <tr>
+                            <td>제목</td>
+                            <td>
+                                <input
+                                    name="boardTitle"
+                                    value={announcement.boardTitle}
+                                    readOnly
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>작성자</td>
+                            <td>
+                                <input
+                                    name="managerId"
+                                    value={announcement.managerId}
+                                    readOnly
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>작성일</td>
+                            <td>
+                                <input
+                                    type="datetime-local"
+                                    name="date"
+                                    value={announcement.boardCreateDate}
+                                    readOnly
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>수정일</td>
+                            <td>
+                                <input
+                                    type="datetime-local"
+                                    name="updateDate"
+                                    value={announcement.boardUpdateDate}
+                                    readOnly
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>내용</td>
+                            <td>
+                                <textarea
+                                    name="boardContent"
+                                    value={announcement.boardContent}
+                                    readOnly
+                                />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <br/>
+                <button type="button" onClick={()=>{handleEditClick(announcement.boardNo)}}>수정하기</button>
+            </form>
+        </>
+    );
+}
+
+export default Anc_DetailForm;
--- ECR/src/main/java/com/project/springboot/repository/AnnouncementRepository.java
+++ ECR/src/main/java/com/project/springboot/repository/AnnouncementRepository.java
@@ -1,5 +1,11 @@
 package com.project.springboot.repository;
 
-public interface AnnouncementRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.springboot.domain.Announcement;
+
+@Repository
+public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
 
 }
